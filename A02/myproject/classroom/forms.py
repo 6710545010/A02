42450baaa7f44bcae74a70424dde76c7530c161a
Napattira Loaklemhung <6710545010@cn102>
2025-09-14 23:53:06 +0700

    forms.py

---
+++ A02/myproject/classroom/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import RoomData, Reservation
+
+class RegistorForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ["room"]
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['room'].queryset = RoomData.objects.filter(room_available=True, room_hour__gt=0)
+
+
+    
+
