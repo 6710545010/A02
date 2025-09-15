2eb286d76258b2b6d9f211579a2454c58398067c
นางสาวนพัตธีรา เหลาเกิ้มหุ่ง <6710545010@eng.tu>
2025-09-15 11:04:31 +0700

    urls.py

--- A02/myproject/classroom/urls.py
+++ A02/myproject/classroom/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path("register/", views.register, name="register"),
-    path("reserve/<int:room_id>", views.reserve_room, name="reserve_room"),
-    path("cancel/", views.cancel_reservation, name="cancel_reservation"),
-    path("login/", auth_views.LoginView.as_view(template_name="rooms/login.html"), name="login"),
+    path("reserve/<int:room_id>", views.reserve_room, name="reserve"),
+    path("cancel/", views.cancel_reservation, name="cancel"),
+    # path("login/", auth_views.LoginView.as_view(template_name="rooms/login.html"), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
 ]
